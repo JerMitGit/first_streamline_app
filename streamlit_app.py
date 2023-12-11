35125609cf1bfb1fc48dd15cf20f0d494db90353
JerMitGit <80525175+JerMitGit@users.noreply.github.com>
2023-12-11 16:36:31 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,5 +24,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
+
+streamlit.header("Fruityvice Fruit Advice!")
 streamlit.text(fruityvice_response)
 
